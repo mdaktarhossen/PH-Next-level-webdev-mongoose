5e3bfb4ab56bbf2fedb686ed7ecbd8a821fffcde
mdaktarhossen <mdaktarhossen20@gmail.com>
2023-05-15 16:01:28 +0600

    post checking done

--- src/apps/modules/user/user.controller.ts
+++ src/apps/modules/user/user.controller.ts
@@ -1,21 +1,22 @@
 import { NextFunction, Request, Response, response } from "express";
-import { createUserToDb } from "./user.services";
+import { createUserToDb, getUsersFromToDb } from "./user.services";
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  const user = await createUserToDb();
+  const data = req.body;
+  const user = await createUserToDb(data);
   res.status(200).json({
     status: "SuccessFull",
     data: user
   })
 };
 
-// export const getUser = async (req: Request, res: Response, next: NextFunction) => {
-//   const user = await getUsersToDb();
-//   res.status(200).json({
-//     status: "SuccessFull",
-//     data: user
-//   })
 
-// }
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  const user = await getUsersFromToDb();
+  res.status(200).json({
+    status: "SuccessFull",
+    data: user
+  })
+};
 
 // Route -> Controllar -> Services
--- src/apps/modules/user/user.services.ts
+++ src/apps/modules/user/user.services.ts
@@ -1,27 +1,14 @@
+import IUser from "./user.interface";
 import { User } from "./user.model";
 
 // import User from "./user.model";
-export const createUserToDb = async () => {
-  const user = await new User({
-    id: "14578",
-    Role: "Student",
-    password: "string",
-    name: {
-      firstName: "akterhs",
-      lastName: "Hosen",
-    },
-    dateOfBirth: "hhh",
-    gender: "mail",
-    email: "ajjs@gmail",
-    contsctNmbr: "01798466491",
-    emergiencyNmbr: "ysgg",
-    presendAdrs: "jshhs",
-    permanandAddress: "jajjs",
-  });
+export const createUserToDb = async (payload: IUser): Promise<IUser> => {
+  const user = await new User(payload);
   await user.save();
   return user;
 };
-// export const getUsersToDb = async () => {
-//   const users = await User.find()
-//   return users;
-// };
+
+export const getUsersFromToDb = async () => {
+  const users = await User.find()
+  return users;
+};
